3f97e207bccdae65a537b526cc2ccddd9a427b38
Seth Unruh <unruhseth317@gmail.com>
2025-02-26 21:48:05 -0700

    Add 'use client' directive to AppCard component

--- nextjs-clerk-app/src/components/AppCard.tsx
+++ nextjs-clerk-app/src/components/AppCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { getImageUrl } from '@/utils/imageUrls';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
