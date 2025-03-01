679e5a5fbe6d5b0d84e067ac91b3c6bbfafd4fb5
Seth Unruh <unruhseth317@gmail.com>
2025-03-01 11:19:10 -0700

    fix: standardize on [app_key] for dynamic routes

--- nextjs-clerk-app/src/app/admin/apps/[app_key]/page.tsx
+++ nextjs-clerk-app/src/app/admin/apps/[app_key]/page.tsx
@@ -1,15 +1,21 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useParams, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
-export default function AppPage() {
-  const { app_key } = useParams();
+interface Props {
+  params: {
+    app_id: string;
+  }
+}
+
+export default function AppPage({ params }: Props) {
+  const { app_id } = params;
   const router = useRouter();
 
   useEffect(() => {
-    router.replace(`/admin/apps/${app_key}/manage`);
-  }, [app_key, router]);
+    router.replace(`/admin/apps/${app_id}/manage`);
+  }, [app_id, router]);
 
   return null;
 } 
