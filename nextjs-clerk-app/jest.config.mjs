5d943523dbec5626a8459aeb202834bdc5fbe386
Seth Unruh <unruhseth317@gmail.com>
2025-03-03 16:45:27 -0700

    fix: move babel config into jest config to enable SWC for production

---
+++ nextjs-clerk-app/jest.config.mjs
@@ -0,0 +1,19 @@
+const customJestConfig = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript',
+        'next/babel'
+      ]
+    }]
+  }
+};
+
+export default customJestConfig; 
